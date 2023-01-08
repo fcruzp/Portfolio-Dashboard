9a4b8409d6e8946dc8956914d5cee0cc06ae208b
Francisco Cruz <fcruzp@gmail.com>
2023-01-08 01:00:26 -0400

    first commit

---
+++ src/utility/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDI0knrBTM-n75QVP6axum3SbNnAC6pYc0",
+    authDomain: "enter-2791c.firebaseapp.com",
+    projectId: "enter-2791c",
+    storageBucket: "enter-2791c.appspot.com",
+    messagingSenderId: "384509229985",
+    appId: "1:384509229985:web:a33c37f1dea7d2825ea9e0",
+    measurementId: "G-47CE14LDB9"
+  }
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+
+  export default firebase
